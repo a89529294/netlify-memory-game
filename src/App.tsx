de2d09c321bd732a2390b8b44b66b1f4a33bd646
Albert Chang <a89529294@hotmail.com>
2022-11-06 16:50:09 +0800

    feat: add cell flip animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import cx from "classix";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GameInfo from "./components/GameInfo";
 
 import Logo from "./components/Logo";
@@ -9,6 +9,11 @@ const cells = new Array(16).fill("");
 export default function App() {
   const [selectedCells, selectCells] = useState<number[]>([]);
 
+  useEffect(() => {
+    if (selectedCells.length === 2) {
+      setTimeout(() => selectCells([]), 2000);
+    }
+  }, [selectedCells]);
   return (
     <main className="grid place-items-center h-screen w-screen bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800">
       <div className="bg-[rgb(21,26,30)] p-6 rounded-lg grid gap-5">
@@ -25,17 +30,24 @@ export default function App() {
           {cells.map((_c, i) => (
             <button
               key={i}
-              className="bg-gray-500 w-32 h-28 rounded-md text-gray-100 hover:bg-cyan-900 hover:text-teal-300 relative"
-              onClick={() => selectedCells.length < 2 && selectCells((pv) => [...pv, i])}>
-              <div className="absolute inset-0 grid place-items-center [backface-visibility:hidden]">
-                <Logo />
-              </div>
+              className=" w-32 h-28 rounded-md text-gray-100 [perspective:1000px]"
+              onClick={() =>
+                selectedCells.length < 2 && selectCells((pv) => [...new Set([...pv, i])])
+              }>
               <div
                 className={cx(
-                  "absolute inset-0 grid place-items-center [backface-visibility:hidden] ",
-                  selectedCells.includes(i) ? "" : "[transform:rotateY(180deg)]"
+                  "[transform-style:preserve-3d] transition-transform duration-300 relative h-full bg-gray-500 hover:bg-cyan-900 hover:text-teal-300 ",
+                  selectedCells.includes(i) ? "[transform:rotateY(180deg)]" : ""
                 )}>
-                <span className="text-6xl">🤩</span>
+                <div className="[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden]">
+                  <Logo />
+                </div>
+                <div
+                  className={cx(
+                    "[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden] [transform:rotateY(180deg)]"
+                  )}>
+                  <span className="text-6xl">🤩</span>
+                </div>
               </div>
             </button>
           ))}
