f910ae7aa81000ab79f4c764c989fce4645f2c73
Albert Chang <a89529294@hotmail.com>
2022-11-19 16:46:05 +0800

    feat: game somewhat playable

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,35 @@ import GameInfo from "./components/GameInfo";
 
 import Logo from "./components/Logo";
 
-const cells = new Array(16).fill("");
+const cells = new Array(16).fill("").map((_, i) => i);
+const emojis = ["😀", "😆", "😂", "🤣", "😍", "🥰", "🤪", "😋", "🥳", "🥶", "😭", "😴", "🤮", "👹"];
+const finalEmojiArray: string[] = [];
+
+for (let i = 0; i < 8; i++) {
+  const randEmojiIdx = Math.floor(Math.random() * emojis.length);
+  const emoji = emojis.splice(randEmojiIdx, 1)[0];
+
+  const randIdx = Math.floor(Math.random() * cells.length);
+  const indexForEmoji1 = cells.splice(randIdx, 1)[0];
+
+  const randIdx2 = Math.floor(Math.random() * cells.length);
+  const indexForEmoji2 = cells.splice(randIdx2, 1)[0];
+
+  finalEmojiArray[indexForEmoji1] = emoji;
+  finalEmojiArray[indexForEmoji2] = emoji;
+}
 
 export default function App() {
-  const [selectedCells, selectCells] = useState<number[]>([]);
-
+  const [selectedCells, selectCells] = useState<{ idx: number; emoji: string }[]>([]);
+  const [correctCells, setCorrectCells] = useState<number[]>([]);
+  const [moves, setMoves] = useState(0);
+  const pairsMatched = correctCells.length / 2;
   useEffect(() => {
     if (selectedCells.length === 2) {
-      setTimeout(() => selectCells([]), 2000);
+      if (selectedCells[0].emoji === selectedCells[1].emoji) {
+        setCorrectCells((pv) => [...pv, selectedCells[0].idx, selectedCells[1].idx]);
+      }
+      setTimeout(() => selectCells([]), 1000);
     }
   }, [selectedCells]);
   return (
@@ -21,36 +42,42 @@ export default function App() {
         <div className="grid grid-cols-4 gap-5">
           <GameInfo label="Pairs matched">
             <h2 className="text-xl">
-              1<span className="text-gray-400 text-base">/8</span>
+              {pairsMatched}
+              <span className="text-gray-400 text-base">/8</span>
             </h2>
           </GameInfo>
           <GameInfo label="Total moves">
-            <h2 className="text-xl">4</h2>
+            <h2 className="text-xl">{moves}</h2>
           </GameInfo>
-          {cells.map((_c, i) => (
-            <button
-              key={i}
-              className=" w-32 h-28 rounded-md text-gray-100 [perspective:1000px]"
-              onClick={() =>
-                selectedCells.length < 2 && selectCells((pv) => [...new Set([...pv, i])])
-              }>
-              <div
-                className={cx(
-                  "[transform-style:preserve-3d] transition-transform duration-300 relative h-full bg-gray-500 hover:bg-cyan-900 hover:text-teal-300 ",
-                  selectedCells.includes(i) ? "[transform:rotateY(180deg)]" : ""
-                )}>
-                <div className="[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden]">
-                  <Logo />
-                </div>
+          {finalEmojiArray.map((emoji, i) => {
+            const flip = selectedCells.map((c) => c.idx).includes(i) || correctCells.includes(i);
+            return (
+              <button
+                key={i}
+                className=" w-32 h-28 rounded-md text-gray-100 [perspective:1000px]"
+                onClick={() => {
+                  if (selectedCells.length >= 2 || correctCells.includes(i)) return;
+                  selectCells((pv) => [...pv, { emoji, idx: i }]);
+                  setMoves((pv) => ++pv);
+                }}>
                 <div
                   className={cx(
-                    "[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden] [transform:rotateY(180deg)]"
+                    "[transform-style:preserve-3d] transition-transform duration-300 relative h-full bg-gray-500 hover:bg-cyan-900 hover:text-teal-300 ",
+                    flip ? "[transform:rotateY(180deg)]" : ""
                   )}>
-                  <span className="text-6xl">🤩</span>
+                  <div className="[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden]">
+                    <Logo />
+                  </div>
+                  <div
+                    className={cx(
+                      "[transform-style:preserve-3d] absolute w-full h-full grid place-items-center [backface-visibility:hidden] [transform:rotateY(180deg)]"
+                    )}>
+                    <span className="text-6xl">{emoji}</span>
+                  </div>
                 </div>
-              </div>
-            </button>
-          ))}
+              </button>
+            );
+          })}
         </div>
       </div>
     </main>
